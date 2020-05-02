0649aa7b42671a4e5da0c5c2075ab867c21e91b3
Александр Евсеев <alexander.evseev@balance-pl.ru>
2020-05-02 06:40:10 +0300

    added methods for login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -18,3 +18,30 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), \
             "Register form is not presented"
+
+    def register_new_user(self, email, password):
+        self.should_be_register_form()
+        self.enter_registration_email(email)
+        self.enter_registration_password(password)
+        self.enter_confirm_password(password)
+        self.click_on_register_button()
+
+    def enter_string_in_field(self, locator, string_for_field):
+        field = self.browser.find_element(*locator)
+        field.click()
+        field.send_keys(string_for_field)
+
+    def enter_registration_email(self, email):
+        self.enter_string_in_field(LoginPageLocators.REGISTRATION_EMAIL_FIELD, email)
+
+    def enter_registration_password(self, password):
+        self.enter_string_in_field(LoginPageLocators.REGISTRATION_PASSWORD_FIELD, password)
+
+    def enter_confirm_password(self, password):
+        self.enter_string_in_field(LoginPageLocators.CONFIRM_PASSWORD_FIELD, password)
+
+    def click_on_register_button(self):
+        register_button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        register_button.click()
+
+
