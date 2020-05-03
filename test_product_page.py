d76ed63205e52fd587449c7cd20eabedd4395ab0
Александр Евсеев <alexander.evseev@balance-pl.ru>
2020-05-03 21:18:18 +0300

    refactored test with parameters

--- test_product_page.py
+++ test_product_page.py
@@ -6,18 +6,12 @@ from .pages.basket_page import BasketPage
 
 
 @pytest.mark.need_review
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                               marks=pytest.mark.xfail(reason="Product name is not match in message")),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link):
+@pytest.mark.parametrize('link_id', [0, 1, 2, 3, 4, 5, 6, pytest.param(7,
+                                                                       marks=pytest.mark.xfail(
+                                                                           reason="Product name is not match in message")),
+                                     8, 9])
+def test_guest_can_add_product_to_basket(browser, link_id):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link_id}'
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_cart()
@@ -107,4 +101,3 @@ class TestUserAddToBasketFromProductPage:
         page.solve_quiz_and_get_code()
         page.should_match_product_name_in_message()
         page.should_match_price_in_message()
-
